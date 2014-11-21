3df1921e113599f10b491a965e43c9ac317845c4
Joseph Dykstra <josephdykstra@gmail.com>
2014-11-21 16:46:22 -0600

    it's mostly a thing

---
+++ index.js
@@ -0,0 +1,85 @@
+var EventEmitter = require('events').EventEmitter
+
+function getNextItem(emitErr, queues, queueKeys) { //emitter, queues, and queueKeys are bound
+	//finds the next user,
+	//removes the next song from their queue and returns/removes it.
+	//Moves that user to the back of the user list
+}
+
+function addItem(emitErr, queues, queueKey, newItem) { //emitter and queues are bound
+	if (queues[queueKey].push && newItem) {
+		queues[queueKey].push(newItem)
+	} else if (!newItem) {
+		emitErr(new TypeError('Invalid newItem: ' + queueKey))
+	} else {
+		emitErr(new TypeError('Invalid queueKey: ' + queueKey))
+	}
+}
+
+function reorder(emitErr, queues, queueKey, thing1, thing2) { //emitter and queues are bound
+	function reorderQueue(newQueue) {
+		if (Array.isArray(newQueue)) {
+			queues[queueKey] = newQueue
+		} else {
+			emitErr(new TypeError('newQueue must be an array'))
+		}
+	}
+
+	function reorderItem(itemId, newIndex) {
+		var queue = queues[queueKey]
+		var oldIndex = queue.map(function (item, index) {
+			return (itemId === item.id) ? index : NaN
+		}).filter(!isNaN)[0]
+
+		if (oldIndex && newIndex) {
+			var cutItem = queue.splice(oldIndex, 1)[0] //cut
+			queue.splice(newIndex + add, 0, cutItem)   //paste
+		}
+		return queue
+	}
+
+	if (typeof thing1 === 'object') {
+		reorderQueue(thing1)
+	} else if (typeof thing1 === 'string' && typeof thing2 === 'number') {
+		var newQueue = reorderItem(thing1, thing2)
+		reorderQueue(newQueue)
+	} else {
+		var args = [].slice.call(arguments, 1).join(', ')
+		emitErr(new TypeError('Invalid arguments passed to reorder( ' + args + ' )'))
+	}
+}
+
+function addKey(emitErr, queues, queueKeys, queueKey, state) { //emitter, queues, and queueKeys are bound
+	if (typeof queueKey === 'string') {
+		queueKeys.push(queueKey)
+		queues[queueKey] = state || []
+	} else {
+		emitErr(new TypeError('queueKey should be a string, but is a ' + typeof queueKey))
+	}
+}
+
+function removeKey(emitErr, queues, queueKeys, queueKey) { //emitter, queues, and queueKeys are bound
+	if (typeof queueKey === 'string') {
+		queueKeys.splice(queueKeys.indexOf(queueKey), 1) //cut from array
+		var state = queues[queueKey]
+	} else {
+		emitErr(new TypeError('queueKey should be a string, but is a ' + typeof queueKey))
+		return null
+	}
+}
+
+module.exports = function closure() {
+	var emitter = new EventEmitter
+	var emitErr = emitter.emit.bind(null, 'error')
+
+	var queueKeys = [] //users
+	var queues = {} //users' queues
+
+	emitter.getNextSong = getNextItem.bind(null, emitErr, queues, queueKeys) // () -> item
+	emitter.addSong =         addItem.bind(null, emitErr, queues)            // (userId, song)
+	emitter.reorderSong =     reorder.bind(null, emitErr, queues)            // (userId, [newly ordered array]) OR (userId, songId, newIndex)
+	emitter.addUser =          addKey.bind(null, emitErr, queues, queueKeys) // (userId, userState)
+	emitter.removeUser =    removeKey.bind(null, emitErr, queues, queueKeys) // (userId) -> userState
+
+	return emitter
+}
--- readme.md
+++ readme.md
@@ -1,15 +1,57 @@
 playlist-combinator
 ===================
 
+Run this code:
+```js
+var playlist = require('playlist-combinator')()
+playlist.on('error', function (err) {
+	console.log(err)
+})
+
+playlist.addUser('josh@example.com', [ { id: 'hash', title: 'whatever' } ])
+playlist.addUser('joseph@example.com')
+
+playlist.addSong('joseph@example.com', { id: 'otherhash, title: 'whatever 2' })
+playlist.addSong('joseph@example.com', { id: 'somemoreid', title: 'o, hi' })
+playlist.addSong('joseph@example.com', { id: 'someid', title: 'thingy' })
+```
+
+Internally:
+```js
+userOrder: ['joseph@example.com', 'josh@example.com']
+songs: {
+	"josh@example.com": [
+		{ id: 'hash', title: 'whatever' }
+	]
+	"joseph@example.com": [
+		{ id: 'otherhash', title: 'whatever 2' },
+		{ id: 'somemoreid', title: 'o, hi' },
+		{ id: 'someid', title: 'thingy' }
+	]
+}
+```
+
+Run this code:
+```js
+var song = playlist.getNextSong() //returns => { id: 'otherhash', title: 'whatever 2' }
+```
+
+Internally:
+```js
+userOrder: ['josh@example.com', 'joseph@example.com'] //note that 'joseph' was moved to the back; it's 'josh's turn next
+songs: {
+	"josh@example.com": [
+		{ id: 'hash', title: 'whatever' }
+	]
+	"joseph@example.com": [
+		{ id: 'somemoreid', title: 'o, hi' },
+		{ id: 'someid', title: 'thingy' }
+	]
+}
+```
+#api
 
 ```js
-var userOrder = ['me@joshduff.com', 'me@josephdykstra.com']
-
-var songs = {
-	"me@joshduff.com": [ { id: 'hash', title: 'whatever' }]
-	"me@josephdyskstra.com": [ {id: 'otherhash', title: 'whatever 2' }]
-}
-
 module.exports = {
 	getNextSong // finds the next user, removes the next song from their queue and returns/removes it.  Moves that user to the back of the user list
 	addSong(userId, song)
